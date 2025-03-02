0e36dc6d68feb8849fa9bb1ad031cf92be855de4
manuelrojas19 <manuelarr99@gmail.com>
2025-03-01 23:00:04 -0600

    feature(async-integration): Adding kafka feature toggle

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { EnterpriseModule } from './modules/enterprises/enterprise.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './config/database/database.module';
 import { PartiesModule } from './modules/parties/parties.module';
+import { Kafka } from 'kafkajs';
+import { KafkaModule } from './config/kafka/kafka.module';
 
 @Module({
   imports: [
@@ -11,6 +13,7 @@ import { PartiesModule } from './modules/parties/parties.module';
       isGlobal: true,
     }),
     DatabaseModule, // Importing encapsulated database configuration
+    KafkaModule,
     EnterpriseModule,
     PartiesModule,
   ],
--- src/config/kafka/kafka.service.ts
+++ src/config/kafka/kafka.service.ts
@@ -1,16 +1,28 @@
-import { Injectable, Inject, OnModuleInit, Logger } from '@nestjs/common';
-import { ClientKafka } from '@nestjs/microservices';
+import { Injectable, OnModuleInit, Logger, Inject } from "@nestjs/common";
+import { ClientKafka } from "@nestjs/microservices";
+
 
 @Injectable()
 export class KafkaService implements OnModuleInit {
   private readonly logger = new Logger(KafkaService.name);
+
   constructor(
     @Inject('KAFKA_SERVICE') private readonly kafkaClient: ClientKafka,
   ) {}
 
   async onModuleInit() {
-    // Establish connection to Kafka
-    await this.kafkaClient.connect();
-    this.logger.log('Kafka started successfully');
+    const kafkaEnabled = process.env.KAFKA_ENABLE === 'true';
+
+    if (kafkaEnabled) {
+      try {
+        // Establish connection to Kafka
+        await this.kafkaClient.connect();
+        this.logger.log('Kafka started successfully');
+      } catch (error) {
+        this.logger.error('Failed to connect to Kafka', error.stack);
+      }
+    } else {
+      this.logger.log('Kafka connection is disabled');
+    }
   }
 }
